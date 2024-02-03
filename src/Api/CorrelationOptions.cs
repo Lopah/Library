59373168239ae9d44fbbfb9dff19171b36a9ccdc
Daniel Jašek <jasekdan@gmail.com>
2024-02-03 18:23:52 +0100

    feat: use correlation middleware

---
+++ src/Api/CorrelationOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Api;
+
+public class CorrelationOptions
+{
+    public bool UseSerilog { get; set; } = false;
+    
+    public string HeaderKey { get; set; } = "CorrelationId";
+    
+    public bool ReplaceTraceIdentifier { get; set; } = true;
+    
+    public bool AddResponseHeader { get; set; } = true;
+    
+    public Func<string> SetCorrelationId { get; set; } = () => Guid.NewGuid().ToString("D");
+}
